a6462bc0308d53d3e4041c0367314928fd7ff6c6
Gizemtokgoz <48003456+Gizemtokgoz@users.noreply.github.com>
2020-09-24 18:40:36 -0700

    invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	static final int WIDTH = 500;
+	static final int HEIGHT = 800;
+	JFrame frame;
+
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+	}
+
+	LeagueInvaders() {
+		frame = new JFrame();
+	}
+
+	void setup() {
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
